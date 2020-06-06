8b28c20105eb92eeff67be5d0f6aba44bf844d07
Trang3598 <nguyenthikieutrang_t61@hus.edu.vn>
2020-06-06 22:22:15 +0700

    list students screen

---
+++ src/View/ListStudents.java
@@ -0,0 +1,122 @@
+package View;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JScrollBar;
+import javax.swing.ImageIcon;
+import javax.swing.JTable;
+
+public class ListStudents extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ListStudents frame = new ListStudents();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ListStudents() {
+		setTitle("Danh s\u00E1ch sinh vi\u00EAn");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1384, 788);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 226, 748);
+		panel.setBackground(new Color(0, 128, 128));
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(0, 128, 128));
+		panel_1.setBounds(0, 0, 226, 231);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("NGUY\u1EC4N TH\u1ECA H\u1ED2NG MINH");
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 14));
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setBounds(20, 190, 206, 30);
+		panel_1.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("New label");
+		lblNewLabel_1.setBounds(10, 11, 194, 168);
+		panel_1.add(lblNewLabel_1);
+		lblNewLabel_1.setIcon(new ImageIcon(ListStudents.class.getResource("/res/avt.png")));
+		
+		JButton btnNewButton = new JButton("Danh s\u00E1ch sinh vi\u00EAn");
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setFont(new Font("Arial", Font.PLAIN, 13));
+		btnNewButton.setBackground(new Color(0, 139, 139));
+		btnNewButton.setBounds(0, 309, 226, 47);
+		panel.add(btnNewButton);
+		
+		JButton btnimDanh = new JButton("\u0110i\u1EC3m danh");
+		btnimDanh.setForeground(Color.WHITE);
+		btnimDanh.setFont(new Font("Arial", Font.PLAIN, 13));
+		btnimDanh.setBackground(new Color(0, 139, 139));
+		btnimDanh.setBounds(0, 358, 226, 47);
+		panel.add(btnimDanh);
+		
+		JButton btnChiaSTi = new JButton("Chia s\u1EBB t\u00E0i li\u1EC7u");
+		btnChiaSTi.setForeground(Color.WHITE);
+		btnChiaSTi.setFont(new Font("Arial", Font.PLAIN, 13));
+		btnChiaSTi.setBackground(new Color(0, 139, 139));
+		btnChiaSTi.setBounds(0, 406, 226, 47);
+		panel.add(btnChiaSTi);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBounds(224, 0, 1156, 56);
+		panel_3.setBackground(new Color(244, 164, 96));
+		contentPane.add(panel_3);
+		panel_3.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(220, 11, 262, 34);
+		panel_3.add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton_1 = new JButton("T\u00ECm ki\u1EBFm ");
+		btnNewButton_1.setForeground(Color.WHITE);
+		btnNewButton_1.setBackground(new Color(0, 128, 128));
+		btnNewButton_1.setBounds(529, 11, 98, 34);
+		panel_3.add(btnNewButton_1);
+		
+		JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(1351, 57, 17, 692);
+		contentPane.add(scrollBar);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setBackground(new Color(135, 206, 235));
+		panel_4.setBounds(224, 57, 1120, 691);
+		contentPane.add(panel_4);
+		panel_4.setLayout(null);
+	}
+}
